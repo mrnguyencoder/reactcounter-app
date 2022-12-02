971f15388a35797d6919516870aec911651ff473
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-02 10:59:13 +0100

    set up add todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+    // short time memory todo
+    const [todos, setTodos] = useState(['coding', 'shopping']);
+
+    const [input, setInput] = useState ('');
+    const addTodo = (event) => {
+        setTodos([...todos, input ]); 
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen flex flex-col justify-center items-center'>
+        <h1 className='text-4xl underline decoration-sky-500 '>
+            Todo CRUD
+        </h1>
+        <input className='my-4 border-y-2'
+            value={input} onChange={event => setInput(event.target.value)}/>
+        <button onClick={addTodo} className='bg-green-600 py-1 px-6 rounded-full'>Add</button>
+        <ul>
+            {/* take from array todos */}
+            {todos.map(todo => (
+                <li>{todo}</li>
+            ))}
+        </ul>
     </div>
-  );
+  )
 }
 
 export default App;
