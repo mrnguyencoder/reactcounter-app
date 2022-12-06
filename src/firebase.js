1fdc33f07aca8836184ae0f8eb34451fb66b15cd
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-06 17:44:53 +0100

    change firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAAyXj4GIOIDL49dcY_55VCaS8zwf_6KgA",
@@ -10,4 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
 
